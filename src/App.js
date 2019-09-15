0eff44d2c43ccc54b28227ff25499692d0f5edee
AvetisDN <avetisdn@gmail.com>
2019-09-15 10:36:00 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from './components/header';
+import LoadingBar from 'react-top-loading-bar';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    state = {
+        loadingBarProgress: 0
+    };
+
+    complete = () => {
+        this.setState({ loadingBarProgress: 100 });
+    };
+    onLoaderFinished = () => {
+        this.setState({ loadingBarProgress: 0 });
+    };
+
+    componentDidMount() {
+        this.complete();
+    }
+
+    render() {
+    return (
+        <div>
+            <LoadingBar
+                progress={this.state.loadingBarProgress}
+                height={3}
+                color='#f11946'
+                onLoaderFinished={() => this.onLoaderFinished()}
+            />
+            <Header/>
+        </div>
+    );
+  }
+
 }
 
 export default App;
